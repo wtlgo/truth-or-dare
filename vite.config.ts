80fa56a09ad3bfcbad7f0d39e6cd0116ba64cb1e
wtlgo <georgeendo0@gmail.com>
2022-07-04 11:32:42 +0300

    Turn into PWA

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,11 @@ import { fileURLToPath, URL } from "url";
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import ViteYaml from "@modyfi/vite-plugin-yaml";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [vue(), ViteYaml()],
+    plugins: [vue(), ViteYaml(), VitePWA({ registerType: "autoUpdate" })],
     resolve: {
         alias: {
             "@": fileURLToPath(new URL("./src", import.meta.url)),
